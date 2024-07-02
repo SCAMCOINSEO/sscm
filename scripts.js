caf32bedbe920a70347fdb3630ddc8fb6b01c62a
SCAMCOINSEO <mat123vey123@gmail.com>
2024-07-02 17:11:39 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -46,7 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
     clickerButton.addEventListener('mousedown', () => {
         this.holdTimer = setTimeout(() => {
             upgradeMenu.style.display = 'block'; // Открываем меню улучшения через 500мс удержания
-        }, 50);
+        }, 250);
     });
 
     clickerButton.addEventListener('mouseup', () => {
