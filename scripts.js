81ce9974ee82711daca7f6e9753357eb362f145f
SCAMCOINSEO <mat123vey123@gmail.com>
2024-07-02 17:44:04 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -46,7 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
     clickerButton.addEventListener('mousedown', () => {
         this.holdTimer = setTimeout(() => {
             upgradeMenu.style.display = 'block'; // Открываем меню улучшения через 500мс удержания
-        }, 300);
+        }, 500);
     });
 
     clickerButton.addEventListener('mouseup', () => {
