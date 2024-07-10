ad6d7116ee2b1ccc6a27427933ae11a18d3ab7d2
SCAMCOINSEO <mat123vey123@gmail.com>
2024-07-11 01:07:16 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -33,8 +33,6 @@ document.addEventListener('DOMContentLoaded', () => {
     function formatCoins(amount) {
         if (amount >= 1000000000) {
             return (amount / 1000000000).toFixed(1) + 'B';
-        } else if (amount >= 1000000) {
-            return (amount / 1000000).toFixed(1) + 'M';
         } else if (amount >= 1000) {
             return (amount / 1000).toFixed(1) + 'K';
         } else {
